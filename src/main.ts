2d2bfd989ce4c8f07bf2268a62b73a0927cfa8c3
nikitamurashka <nikita.murashka@gmail.com>
2024-08-26 10:21:33 +0300

    feat: origin env

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api')
   app.use(cookieParser())
   app.enableCors({
-    origin: ['http://localhost:3001'],
+    origin: [`${process.env.DOMAIN_URL}` || 'http://localhost:3001'],
     credentials: true,
     exposedHeaders:'set-cookie'
   })

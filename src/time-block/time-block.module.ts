21e2e82326f4a0f787f2374098db23898942d0ef
Peter <mr.kaevik@gmail.com>
2024-09-10 21:48:29 +0300

    create: time blocks

--- src/time-block/time-block.module.ts
+++ src/time-block/time-block.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TimeBlockService } from './time-block.service';
 import { TimeBlockController } from './time-block.controller';
+import { PrismaService } from '../prisma.service'
 
 @Module({
   controllers: [TimeBlockController],
-  providers: [TimeBlockService],
+  providers: [TimeBlockService, PrismaService],
 })
 export class TimeBlockModule {}
